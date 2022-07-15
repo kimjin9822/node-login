0eec0d31b303951c1f8bbe937aae0e76d8bc6614
kimjin9822 <kimjin9822@naver.com>
2022-07-15 15:07:12 +0900

    mvc 사용하여 배치

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-"use strict"
+"use strict";
 
 //모듈
 const express = require("express");
 const app = express();
-
-const PORT = 3000;
 
 //라우팅
 const home = require("./routes/home");
@@ -15,6 +13,4 @@ app.set("view engine", "ejs");
 
 app.use("/", home);
 
-app.listen(PORT, () => {
-    console.log("http로 가동된 서버입니다.");
-});
+module.exports = app;
