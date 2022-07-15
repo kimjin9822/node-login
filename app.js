31d642e40adc236cab2850f9190a3866919485f8
kimjin9822 <kimjin9822@naver.com>
2022-07-15 14:36:57 +0900

    로그인 화면

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
+"use strict"
+
+//모듈
 const express = require("express");
 const app = express();
 
+const PORT = 3000;
+
+//라우팅
+const home = require("./routes/home");
+
+//앱 세팅
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    res.render("home/index");
-});
+app.use("/", home);
 
-app.get("/login", (req, res) => {
-    res.render("home/login");
-});
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("http로 가동된 서버입니다.");
 });
