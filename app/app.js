66146337ac9099b055070d2e07850bfaf9907d92
kimjin9822 <kimjin9822@naver.com>
2022-07-22 17:12:11 +0900

    로그인 api 만들기

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 //모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 //라우팅
@@ -11,6 +12,9 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/pubilc`));
+app.use(bodyParser.json());
+// url을 통해 전달되는 데이터에 한글, 공백 같은 문자 포함시 제대로 작동하게 해줌
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use("/", home);
 
