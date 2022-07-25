6c424ad5967bb7a1a82584dd5b7fb289bc403422
kimjin9822 <kimjin9822@naver.com>
2022-07-25 19:21:00 +0900

    재검토

--- app/app.js
+++ app/app.js
@@ -13,6 +13,7 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/pubilc`));
 app.use(bodyParser.json());
+
 // url을 통해 전달되는 데이터에 한글, 공백 같은 문자 포함시 제대로 작동하게 해줌
 app.use(bodyParser.urlencoded({ extended: true}));
 
